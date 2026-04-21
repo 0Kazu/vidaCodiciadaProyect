76e8f7708e3de5518f3b8a3347ae081f41a48bc1
0kazu <diegotello07@gmail.com>
2026-04-21 17:03:50 -0500

    Tratando de mover el personaje

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Velocidad del personaje")]
+    public float speed;
+
+    [Header("Dependencias")]
+    private Vector2 _vectorMovement;
+    private Rigidbody2D _rb;
+
+    void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (_rb != null)
+        {
+            _rb.velocity = speed * _vectorMovement;
+        }
+    }
+
+    public void OnPlayerMovement(InputAction.CallbackContext vector2Dvalue)
+    {
+        _vectorMovement = vector2Dvalue.ReadValue<Vector2>();
+        Debug.Log("Input found");
     }
 }
