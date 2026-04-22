158a9c94a11e76eb9e5aca2d10059dcb2ca834d3
0kazu <diegotello07@gmail.com>
2026-04-22 16:22:17 -0500

    Tratando de trabajar en las habilidades x2

--- Assets/ScriptableObjects/Player/ParticualSkill.cs
+++ Assets/ScriptableObjects/Player/ParticualSkill.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using NaughtyAttributes;
 
-[CreateAssetMenu(fileName = "PlayerSkills", menuName = "Scriptable Objects/Player Skills")]
+[CreateAssetMenu(fileName = "PlayerSkills", menuName = "Scriptable Objects/Particular Skill")]
 public class ParticualSkill : ScriptableObject
 {
     [Header("Skill name")]
     public string nombreDeHabilidad;
 
     public bool puedeAumentarVelocidad;
+    [ShowIf("puedeAumentarVelocidad")]
+    public float aumentoDeVelocidad;
+
+    public bool puedeAumentarResistencia;
+    [ShowIf("puedeAumentarVelocidad")]
+    public float aumentoDeResistencia;
     
 }
